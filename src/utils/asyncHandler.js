c76631d6d1790cc5814fa17fc5f462f5376e56b7
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-05-12 11:51:55 +0530

    changes updated

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,21 @@
-// const asyncHandler = () =>{}
+const asyncHandler = (requesthandler) =>{
+    (req,res,next) =>{
+        Promise.resolve(requesthandler(req,res,next))
+        .catch((err)=> next(err))
+    }
+}
+
 
 export default asyncHandler;
 
-const asyncHandler = (fn)=> async()=>{        //higher order function taking funciton as a parameter
-   
-}
+// const asyncHandler = (fn)=> async(req,res,next)=>{        //higher order function taking funciton as a parameter
+//    try{
+//        await fn(req,res,next)
+//    }
+//    catch (error){
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message : err.message
+//         })
+//    }
+// }
