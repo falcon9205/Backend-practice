cc57fbf7c317fa1c3cbc04bb1864bacb6a839bd3
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-05-24 10:56:38 +0530

    controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import express, { json } from "express"
+
 const app = express()
 
 app.use(cors({
@@ -22,5 +23,15 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 
 app.use(cookieParser())
+// app.listen(process.env.PORT, function(err){
+//     if (err) console.log("Error in server setup")
+//     console.log("Server listening on Port", PORT);
+// })
+//importing routes
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter)
+
+
 
 export default app;
